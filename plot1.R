e1c854091432126b8c63ffbb5b5e66d32b02fccc
josefnatanael <josefnatanael@gmail.com>
2020-06-08 00:36:08 +0700

    plot 1 done, refactored data loading

--- plot1.R
+++ plot1.R
@@ -1,11 +1,11 @@
-# Setup data frame to read
-column_names <- c("date", "time", "globalactivepower", 
-                  "globalreactivepower", "voltage", "globalintensity", 
-                  "submetering1", "submetering2", "submetering3")
-df <- read.table("household_power_consumption.txt", nrows=5000, sep=";", header=F, skip=65000, na.strings="?")
-names(df) <- column_names
-df$date <- as.Date(df$date, "%d/%m/%Y")
-start_date <- as.Date("1/2/2007", "%d/%m/%Y")
-end_date <- as.Date("2/2/2007", "%d/%m/%Y")
-df <- subset(df, df$date %in% c(start_date, end_date))
-rownames(df) <- seq(1:length(df$date))
+source("load_data.R")
+
+# Create the plot
+with(df, 
+     hist(globalactivepower, 
+          main="Global Active Power", 
+          xlab="Global Active Power (kilowatts)", 
+          col="red",
+          ylim=c(0, 1200)))
+dev.copy(png, file = "plot1.png")
+dev.off()
